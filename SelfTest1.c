83c63edc9212aae6a25368230f791e17f4d06d48
tim70036 <tim70036@gmail.com>
2017-06-17 13:05:12 +0800

    Makefile

--- SelfTest1.c
+++ SelfTest1.c
@@ -2,15 +2,12 @@
 
 int main()
 {
-    int a = 2000;
-
-    if(a > 100)
-    {
-        digitalWrite(13, HIGH);
-        delay(a);
-        digitalWrite(13, LOW);
-        delay(a);
-    }
+    int a = 5000;
+    int b = 5000;
+    digitalWrite(13, HIGH);
+    delay(a);
+    digitalWrite(13, LOW);
+    delay(b);
 
     return 0;
 }
